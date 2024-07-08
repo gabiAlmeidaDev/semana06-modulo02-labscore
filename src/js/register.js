5f2a7b270e3ece747c705e53e6e1096c5c8f4cee
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-07-08 19:37:46 -0300

    Exercicio 03 - localstorage

--- src/js/register.js
+++ src/js/register.js
@@ -10,29 +10,6 @@ document.getElementById('cep').addEventListener('blur', function() {
                     document.getElementById('state').value = data.uf;
                 } else {
                     alert('CEP não encontrado!');
-                }
-            })
-            .catch(error => console.error('Erro ao buscar CEP:', error));
-    }
-});
-
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulário enviado com sucesso!');
-});
-
-document.getElementById('cep').addEventListener('blur', function() {
-    const cep = this.value.replace(/\D/g, '');
-    if (cep) {
-        fetch(`https://viacep.com.br/ws/${cep}/json/`)
-            .then(response => response.json())
-            .then(data => {
-                if (!data.erro) {
-                    document.getElementById('street').value = data.logradouro;
-                    document.getElementById('city').value = data.localidade;
-                    document.getElementById('state').value = data.uf;
-                } else {
-                    alert('CEP não encontrado!');
                     clearAddressFields();
                 }
             })
@@ -67,6 +44,7 @@ document.getElementById('registerForm').addEventListener('submit', function(even
         }
     };
 
-    console.log('Dados do aluno:', studentData);
-    alert('Dados salvos com sucesso!');
+    localStorage.setItem('studentData', JSON.stringify(studentData));
+    
+    window.location.href = 'index.html';
 });
